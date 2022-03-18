5627290c7d183b4763935bfdbec1a6887d89c1a2
eric.kindberg01@gmail.com <eric.kindberg01@gmail.com>
2022-03-18 16:21:49 +0100

    18/3

--- Databas 1/Program.cs
+++ Databas 1/Program.cs
@@ -1,15 +1,86 @@
 ﻿using Npgsql;
 using System;
+using System.Data;
 
 namespace Databas_1
 {
     class Program
     {
+        enum State
+        {
+            Start,
+            Logged_in
+
+        }
+
+
         static void Main(string[] args)
         {
+            string connection = "Host=localhost;Username=miint2;Password=wbe4ngqu;Database=MAU";
+            State currentstate;
             Console.WriteLine("Hello World!");
             Databas databas1 = new Databas();
             string selectedDepCode = null;
+            currentstate = State.Logged_in;
+
+            //I början bör man få en meny som frågar om man ska logga in etc.
+            switch (currentstate)
+            {
+                case State.Logged_in:
+                    Console.WriteLine("Please Select one of the options below: " +
+                        "\n 1- Login" +
+                        "\n 2- Register" +
+                        "\n 3- Search for product" +
+                        "\n 4- See all products");
+                    string choice = Console.ReadLine();
+                    if (choice.Equals("1"))
+                    {
+                        Console.WriteLine("Enter your details: (email, password) " +
+                            "\n Note: Use commas(,) between the values");
+
+                        string[] info = Console.ReadLine().Split(",");
+                        using var con = new NpgsqlConnection(connection);
+                        con.Open();
+                        string sql = "call function_username_password(@email, @password)";
+                        using var cmd = new NpgsqlCommand(sql, con);
+                        cmd.Parameters.AddWithValue(parameterName: "@email", info[0]);
+                        cmd.Parameters.AddWithValue(parameterName: "@password", info[1]);
+
+
+                        using NpgsqlDataReader rdr = cmd.ExecuteReader();
+                       // rdr.Read();
+                        //if ( )
+
+                    }
+                    else if (choice.Equals("2"))
+                    {
+                        Console.WriteLine("\n Enter your details first (Email, FirstName, LastName, City, Adress)." +
+                            "\n Note: Use commas(,) between the values.");
+
+                        string[] details = Console.ReadLine().Split(",");
+                        databas1.Add_customer(details[0], details[1], details[2], details[3], details[4]);
+
+                        Console.WriteLine("\n Now choose a password: ");
+
+                        string password = Console.ReadLine();
+                        databas1.Add_password_to_customer(details[0], password);
+
+
+                    }
+
+
+
+                    break;
+                case State.Start:
+
+
+                    break;
+
+
+
+
+            }
+
 
             while (true)
             {
@@ -40,7 +111,7 @@ namespace Databas_1
                         Console.WriteLine("{0} First exception caught.", e);
                     }
                     selectedDepCode = Console.ReadLine();
-                    Console.WriteLine("The selected department is: "+selectedDepCode);
+                    Console.WriteLine("The selected department is: " + selectedDepCode);
                 }
                 else if (choice.Equals("3"))
                 {
@@ -90,14 +161,15 @@ namespace Databas_1
             using var cmd = new NpgsqlCommand(sql, con);
             using NpgsqlDataReader rdr = cmd.ExecuteReader();
 
-                while (rdr.Read())
+            while (rdr.Read())
             {
                 Console.WriteLine("----------------------");
                 Console.WriteLine("ID: " + rdr.GetInt32(ordinal: 0));
                 Console.WriteLine("Name: " + rdr.GetString(ordinal: 1));
                 Console.WriteLine("Department code: " + rdr.GetInt32(ordinal: 2));
                 Console.WriteLine("Salary: " + rdr.GetInt32(ordinal: 3));
-            }   con.Close();
+            }
+            con.Close();
 
 
         }
@@ -115,9 +187,10 @@ namespace Databas_1
             transaction.Commit();
         }
         //------------------------------------////------------------------------------//
-        public void Add_supplier_to_list (string supplier_name, int phonenr, string adress)
+        public void Add_supplier_to_list(string supplier_name, int phonenr, string adress)
         {
             using var con = new NpgsqlConnection(connection);
+            con.Open();
             var transaction = con.BeginTransaction();
             string sentencialSQL = "call add_supplier(@namn,@telnr,@adress)";
             NpgsqlCommand command = new NpgsqlCommand(sentencialSQL, con);
@@ -130,6 +203,7 @@ namespace Databas_1
         public void Add_customer(string email, string f_namn, string l_namn, string city, string adress)
         {
             using var con = new NpgsqlConnection(connection);
+            con.Open();
             var transaction = con.BeginTransaction();
             string sentencialSQL = "call add_customer(@email,@f_namn,@l_namn,@city,@adress)";
             NpgsqlCommand command = new NpgsqlCommand(sentencialSQL, con);
@@ -139,10 +213,12 @@ namespace Databas_1
             command.Parameters.AddWithValue(parameterName: "@city", city);
             command.Parameters.AddWithValue(parameterName: "@adress", adress);
             transaction.Commit();
+            con.Close();
         }
         public void Add_discount(string namn, string supplier_namn, int quantity, int base_price)
         {
             using var con = new NpgsqlConnection(connection);
+            con.Open();
             var transaction = con.BeginTransaction();
             string sentencialSQL = "call add_product(@namn,@supplier_namn,@quantity,@base_price)";
             NpgsqlCommand command = new NpgsqlCommand(sentencialSQL, con);
@@ -153,13 +229,14 @@ namespace Databas_1
             transaction.Commit();
         }
 
-        public void Add_product_to_shoppinglist(string email, string product_namn,string supplier_namn, int quantity)
+        public void Add_product_to_shoppinglist(string email, string product_namn, string supplier_namn, int quantity)
         {
             using var con = new NpgsqlConnection(connection);
+            con.Open();
             var transaction = con.BeginTransaction();
             string sentencialSQL = "call add_product_to_shoppinglist(@email, @product_namn,@supplier_namn,@quantity)";
-            NpgsqlCommand command = new NpgsqlCommand(sentencialSQL,con);
-            command.Parameters.AddWithValue(parameterName: "@email",email);
+            NpgsqlCommand command = new NpgsqlCommand(sentencialSQL, con);
+            command.Parameters.AddWithValue(parameterName: "@email", email);
             command.Parameters.AddWithValue(parameterName: "@prioduct_namn", product_namn);
             command.Parameters.AddWithValue(parameterName: "@supplier_namn", supplier_namn);
             command.Parameters.AddWithValue(parameterName: "@quantity", quantity);
@@ -169,6 +246,7 @@ namespace Databas_1
         public void Add_shoppinglist_to_order(int shoplist_id)
         {
             using var con = new NpgsqlConnection(connection);
+            con.Open();
             var transaction = con.BeginTransaction();
             string sentencialSQL = "call add_shoplist_to_order(@sl_id)";
             NpgsqlCommand command = new NpgsqlCommand(sentencialSQL, con);
@@ -178,6 +256,7 @@ namespace Databas_1
         public void delete_product_not_sold(string product_namn)
         {
             using var con = new NpgsqlConnection(connection);
+            con.Open();
             var transaction = con.BeginTransaction();
             string sentencialSQL = "call delete_product_not_sold(@product_namn)";
             NpgsqlCommand command = new NpgsqlCommand(sentencialSQL, con);
@@ -188,6 +267,7 @@ namespace Databas_1
         public void Update_product_quantity(string product_namn, double variation)
         {
             using var con = new NpgsqlConnection(connection);
+            con.Open();
             var transaction = con.BeginTransaction();
             string sentencialSQL = "call update_product_quantity(@product_namn,@variation)";
             NpgsqlCommand command = new NpgsqlCommand(sentencialSQL, con);
@@ -256,7 +336,7 @@ namespace Databas_1
                 else
                     SQL = "Select * from discount where discount.id = " + id;
             }
-            else    SQL = "Select * from discount";
+            else SQL = "Select * from discount";
 
             using var cmd = new NpgsqlCommand(SQL, con);
             using NpgsqlDataReader rdr = cmd.ExecuteReader();
@@ -269,9 +349,16 @@ namespace Databas_1
             }
             con.Close();
         }
-        public void Select_Order()
+       public void Add_password_to_customer(string email, string password)
         {
-
+            using var con = new NpgsqlConnection(connection);
+            con.Open();
+            var transaction = con.BeginTransaction();
+            string sentencialSQL = "call add_password_to_customer(@email, @password)";
+            NpgsqlCommand command = new NpgsqlCommand(sentencialSQL, con);
+            command.Parameters.AddWithValue(parameterName: "@email",email);
+            command.Parameters.AddWithValue(parameterName: "@password",password);
+            transaction.Commit();
         }
 
 
